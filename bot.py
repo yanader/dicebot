153e2a152a5bd76fd2efc712ddedd8b9188c77f0
yanader <mrbarrow@hotmail.co.uk>
2023-04-09 21:28:37 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'TOKEN HERE'
+    TOKEN = token
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
